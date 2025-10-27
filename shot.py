8eaa24c28b735f2330bf07083a08e1fe45c4cc43
Greg <199975660+Gregypo085@users.noreply.github.com>
2025-10-27 17:18:49 -0400

    added shot to self kill once it's radius is off screen

--- shot.py
+++ shot.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import SHOT_RADIUS
+from constants import SHOT_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Shot(CircleShape):
     def __init__(self, x, y):
@@ -18,5 +18,15 @@ class Shot(CircleShape):
             self.kill()
             return
 
+        # move
         self.position += self.velocity * dt
-        self.wrap_position()
+
+        # If the shot leaves the screen fully (including its radius), remove it.
+        if (
+            self.position.x < -self.radius
+            or self.position.x > SCREEN_WIDTH + self.radius
+            or self.position.y < -self.radius
+            or self.position.y > SCREEN_HEIGHT + self.radius
+        ):
+            self.kill()
+            return
